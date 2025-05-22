1412232f18990f3f58c178e10b92e8f1dcde78ed
Tubaarpacay <tubaarpacay@posta.mu.edu.tr>
2025-05-22 12:52:29 +0300

    week11

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
+//import java.util.Iterator;
+//import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
+//import java.util.Stack;
+public class Main {
+    public static void main(String[] args)  {
+        /*
+        Stack stack=new StackArray();
+        try{
+            stack.pop();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+         */
+        Stack stack=new StackLinkedList();
+        try{
+            stack.pop();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        /*
+        ArrayList<String> stringArrayList= new ArrayList<String>();
+        stringArrayList.add("String");
+
+        HashMap<String , Double> grades=new HashMap<>();
+        grades.put("Ali",35.0);
+        System.out.println(grades);
+        grades.put("Pelin",90.0);
+        System.out.println(grades);
+        grades.put("Enes",10.0);
+        System.out.println(grades);
+        grades.put("Ali",60.0);
+        System.out.println(grades);
+        System.out.println(grades.keySet().getClass());
+        System.out.println(grades.keySet() instanceof Set);
+        */
+        /*
         Stack stack = new StackLinkedList();
         stack.push("This is a string");
         stack.push(5);
@@ -10,5 +50,6 @@ public class Main {
         System.out.println("Pop = " +
                 stack.pop());
         stack.peek();
+        */
     }
 }
--- src/StackArray.java
+++ src/StackArray.java
@@ -10,7 +10,10 @@ public class StackArray implements Stack{
         stack.add(0,item);
     }
     @Override
-    public Object pop() {
+    public Object pop() throws  MyEmptyStackException {
+        if(this.isEmpty()){
+            throw new MyEmptyStackException("Stack is Empty, you cannot pop anything from an Empty Stack ");
+        }
         return stack.remove(0);
     }
     @Override
